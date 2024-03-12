da30512f2018563681c42801ed0c0fd559856a6e
rohitroyny <157422306+rohitroyny@users.noreply.github.com>
2024-03-11 21:40:05 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,10 +10,30 @@ use app\controllers\MainController;
 use app\controllers\UserController;
 use app\controllers\PostController;
 
-
 $url = $_SERVER["REQUEST_URI"];
 
-//todo add a switch statement router to route based on the url
-//if it is "/posts" return an array of posts via the post controller
-//if it is "/" return the homepage view from the main controller
-//if it is something else return a 404 view from the main controller
+// Create instances of the controllers
+$mainController = new MainController();
+$postController = new PostController();
+$userController = new UserController();
+
+// Parse the URL to get the path only, excluding query string
+$urlPath = parse_url($url, PHP_URL_PATH);
+
+// Routing switch statement
+switch ($urlPath) {
+    case "/posts":
+        // Return an array of posts via the post controller
+        echo json_encode($postController->getPosts());
+        break;
+    case "/":
+        // Return the homepage view from the main controller
+        $mainController->index();
+        break;
+    default:
+        // Return a 404 view from the main controller
+        $mainController->pageNotFound();
+        break;
+}
+
+// Implement the methods getPosts, index, and pageNotFound in their respective controllers.
